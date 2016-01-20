3a657fbefa2686e2ac3503abb4ea16fff9b0e30a
MacarenaGomez <emgomezcarmona@gmail.com>
2016-01-20 08:27:47 +0100

    Eliminada una migration de más

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117215246) do
+ActiveRecord::Schema.define(version: 20160117183244) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -42,10 +42,6 @@ ActiveRecord::Schema.define(version: 20160117215246) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
